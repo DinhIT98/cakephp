5456fb0400931881869f12d5a5270e1d058152bd
nguyen huu dinh <ndinh725@gmail.com>
2020-11-17 13:38:37 +0700

    add file

---
+++ config/database.php
@@ -0,0 +1,13 @@
+class DATABASE_CONFIG {
+
+public $development = array(
+    'datasource' => 'Database/Mysql',
+    'persistent' => false,
+    'host' => 'localhost',
+    'login' => 'root',
+    'password' => '',
+    'database' => 'demo',
+    'prefix' => '',
+    'encoding' => 'utf8',
+);
+}
